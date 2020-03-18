29612ccce46f8916bea21d3f578235c6cfed1ffe
Juan Fernandez <jcfernan@live.com>
2020-03-18 21:02:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,11 +1,38 @@
-function dwarfRollCall(dwarves) {
+var dwarfs = ["Doc", "Dopey", "Bashful", "Grumpy"];
+
+function dwarfRollCall(dwarves, counter) {
+  var array = [];
+  for (var i=0; i<dwarves.length; i++){
+    array.push(`${i+1}. ${dwarves[i]} `);
+  }
+  return array.join("");
 }
 
+var planeteerCalls = ["earth", "wind", "fire", "water", "heart"];
+
 function summonCaptainPlanet(planeteerCalls){
+  return (planeteerCalls.toUpperCase() + "!");
 }
 
 function longPlaneteerCalls(words) {
+  for (var i=0; i<words.length; i++){
+    if (words[i].length >4){
+      return true;
+    }
+  }
+return false;
 }
 
 function findTheCheese (foods) {
+for (var i=0; i<foods.length; i++){
+  if (foods[i] == "cheddar"
+  || foods[i] =="gouda"
+  || foods[i] =="camembert"
+  || foods[i] == "swiss"
+  ){
+  return foods[i];
 }
+}
+return "no cheese!";
+}
+
